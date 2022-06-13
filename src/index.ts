5dad0d8e7bb9c2b7ae0cf828938728e6aa5c8f34
Budi Santoso <budisantoso@Budis-MacBook-Pro.local>
2022-06-13 14:29:43 +0700

    initial react branch commit

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,4 @@ const root = document.getElementById("root");
 
 root!.innerHTML = "want to being react app!";
 
-const mekarSariManagement: string = "ini string mekarSariManagement";
-
 console.log(obj);
